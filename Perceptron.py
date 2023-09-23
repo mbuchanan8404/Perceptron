ec029720fc32968104ac6036ca82ee57c8ae9ebf
Matthew Buchanan <30788167+mbuchanan8404@users.noreply.github.com>
2023-09-23 09:53:39 -0500

    Add files via upload

---
+++ Perceptron.py
@@ -0,0 +1,86 @@
+#  Matthew Buchanan
+#  Perceptron Project
+#  Intro to Neural Networks
+#  Fall 2022
+
+
+class Perceptron(object):
+
+    # Create a new Perceptron
+    #
+    # Params:	bias -	arbitrarily chosen value that affects the overall output
+    #					regardless of the inputs
+    #
+    #			synaptic_weights -	list of initial synaptic weights for this Perceptron
+    def __init__(self, bias, synaptic_weights):
+        self.bias = bias
+        self.synaptic_weights = synaptic_weights
+
+    # Activation function
+    # Quantizes the induced local field
+    #
+    # Params:	z - the value of the induced local field
+    #
+    # Returns:	an integer that corresponds to one of the two possible output values (usually 0 or 1)
+    def activation_function(self, z):
+        return 1 if z >= 0.0 else 0
+
+    # Compute and return the weighted sum of all inputs (not including bias)
+    #
+    # Params:	inputs - a single input vector (which may contain multiple individual inputs)
+    #
+    # Returns:	a float value equal to the sum of each input multiplied by its
+    #			corresponding synaptic weight
+    def weighted_sum_inputs(self, inputs):
+        weighted_inputs = 0.0
+        for i in range(len(inputs) - 1):
+            weighted_inputs += inputs[i] * self.synaptic_weights[i]
+        return weighted_inputs
+
+    # Compute the induced local field (the weighted sum of the inputs + the bias)
+    #
+    # Params:	inputs - a single input vector (which may contain multiple individual inputs)
+    #
+    # Returns:	the sum of the weighted inputs adjusted by the bias
+    def induced_local_field(self, inputs):
+        return self.bias + self.weighted_sum_inputs(inputs)
+
+    # Predict the output for the specified input vector
+    #
+    # Params:	input_vector - a vector or row containing a collection of individual inputs
+    #
+    # Returns:	an integer value representing the final output, which must be one of the two
+    #			possible output values (usually 0 or 1)
+    def predict(self, input_vector):
+        return self.activation_function(self.induced_local_field(input_vector))
+
+    # Train this Perceptron
+    #
+    # Params:	training_set        - a collection of input vectors that represents a subset of the entire dataset
+    #			learning_rate       - the amount to adjust the synaptic weights following an incorrect prediction
+    #			number_of_epochs    - the number of times the entire training set is processed by the perceptron
+    #
+    # Returns:	no return value
+    def train(self, training_set, learning_rate, number_of_epochs):
+        # zero all the synaptic weights
+        for i in range(len(self.synaptic_weights)):
+            self.synaptic_weights[i] = 0.0
+        for epoch in range(number_of_epochs):
+            for i in range(len(training_set)):
+                prediction = self.predict(training_set[i])
+                expected = training_set[i][60]
+                error = expected - prediction
+                self.bias = self.bias + learning_rate * error
+                for j in range(len(self.synaptic_weights)):
+                    self.synaptic_weights[j] = self.synaptic_weights[j] + learning_rate * error * training_set[i][j]
+
+    # Test this Perceptron
+    #
+    # Params:	test_set - the set of input vectors to be used to test the perceptron after it has been trained
+    #
+    # Returns:	a collection or list containing the actual output (predictions) for each input vector
+    def test(self, test_set):
+        predictions = list()
+        for i in range(len(test_set)):
+            predictions.append(self.predict(test_set[i]))
+        return predictions
---
+++ Perceptron_Tester.py
@@ -0,0 +1,115 @@
+#  Matthew Buchanan
+#  Perceptron Project
+#  Intro to Neural Networks
+#  Fall 2022
+
+
+from csv import reader  # reader object reads a csv file line by line
+from random import sample  # used to randomly sample a list without replacement
+from Perceptron import Perceptron  # this is the Perceptron class in the Perceptron.py file
+
+
+######################################################################
+##### DATASET FUNCTIONS                                          #####
+######################################################################
+
+# Load the CSV file containing the inputs and desired outputs
+#
+#	dataset is a 2D matrix where each row contains 1 set of inputs plus the desired output
+#		-for each row, columns 0-59 contain the inputs as floating point values
+#		-column 60 contains the desired output as a character: 'R' for Rock or 'M' for Metal
+#		-all values will be string values; conversion to appropriate types will be necessary
+#		-no bias value is included in the data file
+def load_csv(filename):
+    # data will be the matrix containing the inputs
+    data = list()
+    # Standard Python code to read each line of text from the file as a row
+    with open(filename, 'r') as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            if not row:
+                continue
+            # add current row to data
+            data.append(row)
+    return data
+
+
+# Convert the input values in the specified column of the dataset from strings to floats
+def convert_inputs_to_float(data, column):
+    for row in data:
+        row[column] = float(row[column].strip())
+
+
+# Convert the desired output values, located in the specified column, to unique integers
+# For 2 classes of outputs, 1 desired output will be 0, the other will be 1
+def convert_desired_outputs_to_int(data, column):
+    # Enumerate all the values in the specified column for each row
+    class_values = [row[column] for row in data]
+    # Create a set containing only the unique values
+    unique = set(class_values)
+    # Create a lookup table to map each unique value to an integer (either 0 or 1)
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = i
+    # Replace the desired output string values with the corresponding integer values
+    for row in data:
+        row[column] = lookup[row[column]]
+    return lookup
+
+
+# Load the dataset from the CSV file specified by filename
+def load_dataset(filename):
+    # Read the data from the specified file
+    data = load_csv(filename)
+    # Convert all the input values from strings to floats
+    for column in range(len(data[0]) - 1):
+        convert_inputs_to_float(data, column)
+    # Convert the desired outputs from strings to ints
+    convert_desired_outputs_to_int(data, len(data[0]) - 1)
+    return data
+
+######################################################################
+##### CREATE THE TRAINING SET                                    #####
+######################################################################
+
+# Create the training set
+#	-Training set will consist of a random sampling without replacement of the data set
+#
+# Params:	data - the entire dataset
+#
+# Returns:	a matrix, or list of rows, containing only a subset of the input
+#			vectors from the entire dataset
+def create_training_set(data):
+    return sample(data, 75)
+
+######################################################################
+##### CREATE A PERCEPTRON, TRAIN IT, AND TEST IT                 #####
+######################################################################
+
+# Step 1: Acquire the dataset
+dataset = load_dataset('sonar_all-data.csv')
+
+# Step 2: Create the training set
+training_set = create_training_set(dataset)
+
+# Step 3: Create the perceptron
+bias = 1.0
+weights = list(0.1 for i in range(60))
+perceptron = Perceptron(bias, weights)
+
+# Step 4: Train the perceptron
+learning_rate = 0.01
+number_of_epochs = 500
+perceptron.train(training_set, learning_rate, number_of_epochs)
+
+# Step 5: Test the trained perceptron
+predictions = perceptron.test(dataset)
+
+# Step 6: Display the test results and accuracy of the perceptron
+error_rate = 0.0
+for i in range(len(predictions)):
+    print('Actual: ' + str(predictions[i]) + '  Expected: ' + str(dataset[i][60]))
+    if predictions[i] != dataset[i][60]:
+        error_rate += 1.0
+error_rate = error_rate / len(dataset)
+print('\nError Rate: ' + str(round(error_rate * 100, 2)) + '%')
